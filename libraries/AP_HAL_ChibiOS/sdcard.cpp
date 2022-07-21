5cbf26786942f92ef2dda20c7918b18854335880
bugobliterator <siddharthbharatpurohit@gmail.com>
2022-07-22 05:17:43 +0530

    AP_HAL_ChibiOS: add block filesystem access support when using MSD USB

--- libraries/AP_HAL_ChibiOS/sdcard.cpp
+++ libraries/AP_HAL_ChibiOS/sdcard.cpp
@@ -21,8 +21,20 @@
 #include <AP_BoardConfig/AP_BoardConfig.h>
 #include <AP_Filesystem/AP_Filesystem.h>
 #include "hwdef/common/usbcfg.h"
+#include "stm32_util.h"
 
 extern const AP_HAL::HAL& hal;
+
+
+static void block_filesys_access()
+{
+    AP::FS().block_access();
+}
+
+static void free_filesys_access()
+{
+    AP::FS().free_access();
+}
 
 #ifdef USE_POSIX
 static FATFS SDC_FS; // FATFS object
@@ -117,7 +129,7 @@ bool sdcard_init()
     #else
                     &USBD2,
     #endif
-                    (BaseBlockDevice*)&SDCD1, blkbuf, txbuf,  NULL, NULL);
+                    (BaseBlockDevice*)&SDCD1, blkbuf, txbuf,  NULL, NULL, block_filesys_access, free_filesys_access);
             usbDisconnectBus(serusbcfg1.usbp);
             usbStop(serusbcfg1.usbp);
             chThdSleep(chTimeUS2I(1500));
