xianyongsheng/waf

Make error messages more useful
Just convert the list by joining the elements with space. It may not be
perfect since it may explode in cases we have spaces, but it's better
than giving a list that you can't do anything with it.

By adding the error output as a command one can manually build that
single compilation unit to understand better the error and even switch
to another compile to get a better error message.

waflib/Task.py
@@ -411,10 +411,15 @@ class TaskBase(evil):
 		elif not self.hasrun:
 			return 'task in %r was not executed for some reason: %r' % (name, self)
 		elif self.hasrun == CRASHED:
+			if isinstance(msg, str):
+				txt = cmd
+			else:
+				txt = ' '.join(repr(x) if ' ' in x else x for x in msg)
+
 			try:
-				return ' -> task in %r failed (exit status %r): %r\n%r' % (name, self.err_code, self, msg)
+				return ' -> task in %r failed (exit status %r): %r\n%s' % (name, self.err_code, self, txt)
 			except AttributeError:
-				return ' -> task in %r failed: %r\n%r' % (name, self, msg)
+				return ' -> task in %r failed: %r\n%s' % (name, self, txt)
 		elif self.hasrun == MISSING:
 			return ' -> missing files in %r: %r\n%r' % (name, self, msg)
 		else:

bugobliterator/ardupilot

waflib/Task.py
@@ -458,10 +458,15 @@ class Task(evil):
 		elif not self.hasrun:
 			return 'task in %r was not executed for some reason: %r' % (name, self)
 		elif self.hasrun == CRASHED:
+			if isinstance(msg, str):
+				txt = msg
+			else:
+				txt = ' '.join(repr(x) if ' ' in x else x for x in msg)
+
 			try:
-				return ' -> task in %r failed with exit status %r%s' % (name, self.err_code, msg)
+				return ' -> task in %r failed (exit status %r): %r\n%s' % (name, self.err_code, self, txt)
 			except AttributeError:
-				return ' -> task in %r failed%s' % (name, msg)
+				return ' -> task in %r failed: %r\n%s' % (name, self, txt)
 		elif self.hasrun == MISSING:
 			return ' -> missing files in %r%s' % (name, msg)
 		elif self.hasrun == CANCELED: