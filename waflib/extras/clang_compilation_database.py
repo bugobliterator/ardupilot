a0abd3cb35ff0e0a54b93edd47abb026a46f9ada
bugobliterator <siddharthbharatpurohit@gmail.com>
2024-06-05 21:18:02 +1000

    fix generation of compile_commands.json file

--- waflib/extras/clang_compilation_database.py
+++ waflib/extras/clang_compilation_database.py
@@ -29,90 +29,86 @@ from waflib import Logs, TaskGen, Task, Build, Scripting
 
 Task.Task.keep_last_cmd = True
 
+def write_compilation_database(bld):
+	"""
+	Write the clang compilation database as JSON
+	"""
+	database_file = bld.bldnode.make_node('compile_commands.json')
+	Logs.info('Build commands will be stored in %s', database_file.path_from(bld.path))
+	try:
+		root = database_file.read_json()
+	except IOError:
+		root = []
+	clang_db = dict((x['file'], x) for x in root)
+	for task in bld.clang_compilation_database_tasks:
+		try:
+			cmd = task.last_cmd
+		except AttributeError:
+			continue
+		f_node = task.inputs[0]
+		filename = f_node.path_from(task.get_cwd())
+		entry = {
+			"directory": task.get_cwd().abspath(),
+			"arguments": cmd,
+			"file": filename,
+		}
+		clang_db[filename] = entry
+	root = list(clang_db.values())
+	database_file.write_json(root)
+
+def dry_run_compilation_database(bld):
+	"""
+	Build dry run
+	"""
+	bld.clang_compilation_database_tasks = []
+	# we need only to generate last_cmd, so override
+	# exec_command temporarily
+	def exec_command(bld, *k, **kw):
+		return 0
+
+	for g in bld.groups:
+		for tg in g:
+			if bld.targets not in tg.name:
+				continue
+			try:
+				f = tg.post
+			except AttributeError:
+				pass
+			else:
+				f()
+
+			if isinstance(tg, Task.Task):
+				lst = [tg]
+			else: lst = tg.tasks
+			for tsk in lst:
+				if tsk.__class__.__name__ == "swig":
+					tsk.runnable_status()
+					if hasattr(tsk, 'more_tasks'):
+						lst.extend(tsk.more_tasks)
+				# Not all dynamic tasks can be processed, in some cases
+				# one may have to call the method "run()" like this:
+				#elif tsk.__class__.__name__ == 'src2c':
+				#	tsk.run()
+				#	if hasattr(tsk, 'more_tasks'):
+				#		lst.extend(tsk.more_tasks)
+
+				tup = tuple(y for y in [Task.classes.get(x) for x in ('c', 'cxx')] if y)
+				if isinstance(tsk, tup):
+					bld.clang_compilation_database_tasks.append(tsk)
+					tsk.nocache = True
+					old_exec = tsk.exec_command
+					tsk.exec_command = exec_command
+					tsk.run()
+					tsk.exec_command = old_exec
+
+	write_compilation_database(bld)
+
 class ClangDbContext(Build.BuildContext):
 	'''generates compile_commands.json by request'''
 	cmd = 'clangdb'
 
-	def write_compilation_database(self):
-		"""
-		Write the clang compilation database as JSON
-		"""
-		database_file = self.bldnode.make_node('compile_commands.json')
-		Logs.info('Build commands will be stored in %s', database_file.path_from(self.path))
-		try:
-			root = database_file.read_json()
-		except IOError:
-			root = []
-		clang_db = dict((x['file'], x) for x in root)
-		for task in self.clang_compilation_database_tasks:
-			try:
-				cmd = task.last_cmd
-			except AttributeError:
-				continue
-			f_node = task.inputs[0]
-			filename = f_node.path_from(task.get_cwd())
-			entry = {
-				"directory": task.get_cwd().abspath(),
-				"arguments": cmd,
-				"file": filename,
-			}
-			clang_db[filename] = entry
-		root = list(clang_db.values())
-		database_file.write_json(root)
-
 	def execute(self):
-		"""
-		Build dry run
-		"""
-		self.restore()
-		self.cur_tasks = []
-		self.clang_compilation_database_tasks = []
-
-		if not self.all_envs:
-			self.load_envs()
-
-		self.recurse([self.run_dir])
-		self.pre_build()
-
-		# we need only to generate last_cmd, so override
-		# exec_command temporarily
-		def exec_command(self, *k, **kw):
-			return 0
-
-		for g in self.groups:
-			for tg in g:
-				try:
-					f = tg.post
-				except AttributeError:
-					pass
-				else:
-					f()
-
-				if isinstance(tg, Task.Task):
-					lst = [tg]
-				else: lst = tg.tasks
-				for tsk in lst:
-					if tsk.__class__.__name__ == "swig":
-						tsk.runnable_status()
-						if hasattr(tsk, 'more_tasks'):
-							lst.extend(tsk.more_tasks)
-					# Not all dynamic tasks can be processed, in some cases
-					# one may have to call the method "run()" like this:
-					#elif tsk.__class__.__name__ == 'src2c':
-					#	tsk.run()
-					#	if hasattr(tsk, 'more_tasks'):
-					#		lst.extend(tsk.more_tasks)
-
-					tup = tuple(y for y in [Task.classes.get(x) for x in ('c', 'cxx')] if y)
-					if isinstance(tsk, tup):
-						self.clang_compilation_database_tasks.append(tsk)
-						tsk.nocache = True
-						old_exec = tsk.exec_command
-						tsk.exec_command = exec_command
-						tsk.run()
-						tsk.exec_command = old_exec
-
-		self.write_compilation_database()
+		dry_run_compilation_database(self)
 
 EXECUTE_PATCHED = False
 def patch_execute():
@@ -125,13 +121,13 @@ def patch_execute():
 		"""
 		Invoke clangdb command before build
 		"""
-		if self.cmd.startswith('build'):
-			Scripting.run_command(self.cmd.replace('build','clangdb'))
-
+		dry_run_compilation_database(self)
+		setattr(Build.BuildContext, 'compile', old_execute_build)
 		old_execute_build(self)
 
-	old_execute_build = getattr(Build.BuildContext, 'execute_build', None)
-	setattr(Build.BuildContext, 'execute_build', new_execute_build)
+	# this ensures the new_execute_build right before compile build
+	old_execute_build = getattr(Build.BuildContext, 'compile', None)
+	setattr(Build.BuildContext, 'compile', new_execute_build)
 	EXECUTE_PATCHED = True
 
 patch_execute()
